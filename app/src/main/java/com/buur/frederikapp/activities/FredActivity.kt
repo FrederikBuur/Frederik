cf910453a685a64c80f713b9ceeb5b7140320a0d
Unknown <frederik_buur@hotmail.com>
2018-05-05 16:28:23 +0200

    Set up basic stuff

---
+++ app/src/main/java/com/buur/frederikapp/activities/FredActivity.kt
@@ -0,0 +1,4 @@
+package com.buur.frederikapp.activities
+
+class FredActivity {
+}
---
+++ app/src/main/java/com/buur/frederikapp/activities/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.buur.frederikapp.activities.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.buur.frederikapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
