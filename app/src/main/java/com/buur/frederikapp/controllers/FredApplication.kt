37119fcdc5a51dee17cac0f55289db1862e65a29
Unknown <frederik_buur@hotmail.com>
2018-06-16 16:37:54 +0200

    Can now read in realm db via : chrome://inspect/#devices
    chrome://inspect/#devices

--- app/src/main/java/com/buur/frederikapp/controllers/FredApplication.kt
+++ app/src/main/java/com/buur/frederikapp/controllers/FredApplication.kt
@@ -2,14 +2,14 @@ package com.buur.frederikapp.controllers
 
 import android.app.Application
 import android.content.Context
-import android.media.MediaCas
 import com.buur.frederikapp.BuildConfig
 import com.buur.frederikapp.R
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
+import com.facebook.stetho.Stetho
 import com.google.android.gms.ads.MobileAds
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider
 import io.fabric.sdk.android.Fabric
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
@@ -24,8 +24,20 @@ class FredApplication : Application() {
         // AdMob
         MobileAds.initialize(this, getString(R.string.admob_app_id))
 
+        // setup Stetho - chrome://inspect/#devices
+        setupStetho()
 
         initFabric(this)
+    }
+
+    private fun setupStetho() {
+        if (BuildConfig.DEBUG) {
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                            .build())
+        }
     }
 
     private fun setupRealm() {
